9374b8805b6e7d3eb3b678bcb15925244f5acb61
Ningze Dai <ningzedai@ningzes-mbp.lan>
2022-11-07 08:27:50 -0500

    login view ui

--- geotag/ContentView.swift
+++ geotag/ContentView.swift
@@ -9,10 +9,67 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        NavigationView {
-            Text("Select an item")
+        ZStack {
+            Color.green
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300)
+                    .padding(.bottom,40)
+                
+                VStack {
+                    TextField("Username",text: $username)
+                            .font(Font.system(size: 30))
+                            .foregroundColor(.gray)
+                            .frame(width: 300, height: 50)
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                       Divider()
+                        .frame(width: 296)
+                        //.background(Color.white)
+                    
+                    TextField("Password", text: $password)
+                            .font(Font.system(size: 30))
+                            .foregroundColor(Color.black)
+                           // .background(Color.white)
+                            .frame(width: 300, height: 50)
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                }
+                .background(.white)
+                .cornerRadius(5)
+
+               
+                                        
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .foregroundColor(Color.white)
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        //.cornerRadius(20.0)
+                        .padding(.top, 10)
+                }
+
+                
+                
+                
+            }
+            
+            Spacer()
+            
         }
+    
+        
     }
     
 }
